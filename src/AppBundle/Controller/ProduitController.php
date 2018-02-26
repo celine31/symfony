ecf7c8f4b185d7a2c9f2369338462df3727031cf
celine31 <dadou20231@gmail.com>
2018-02-26 10:23:37 +0100

    route4 suppression du produit en fonction de l'id

--- src/AppBundle/Controller/ProduitController.php
+++ src/AppBundle/Controller/ProduitController.php
@@ -26,16 +26,49 @@ class ProduitController extends Controller {
     }
 
     /**
-     * @Route("/produit2/{id_produit}")
+     * @Route("/produit2/{id_produit}", name="produit2")
      */
     public function produit2Action($id_produit) {
-        if (filter_var($id_produit, FILTER_VALIDATE_INT, ['options'=>['min_range'=>1]])) {
+        if (filter_var($id_produit, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]])) {
             $produit = $this->getDoctrine()->getManager()->getRepository("AppBundle:Produit")->find($id_produit);
             if ($produit) {
-                return $this->render('produit2.html.twig', ['produit'=>$produit]); //chemin à partir de twig
+                return $this->render('produit2.html.twig', ['produit' => $produit]); //chemin à partir de twig
             }
         }
-     throw $this->createNotFoundException("Produit indisponible");
+        throw $this->createNotFoundException("Produit indisponible");
     }
 
+    /**
+     * @Route("/produit3/{id_produit}")
+     */
+    public function produit3Action($id_produit) {
+        if (filter_var($id_produit, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]])) {
+            $produit = $this->getDoctrine()->getRepository("AppBundle:Produit")->find($id_produit);
+            if ($produit) {
+                $produit = $produit->setPrix($produit->getPrix() * 1.1);
+                $this->getDoctrine()->getManager()->flush();
+                //return $this->forward('produit2',['id_produit' => $id_produit]); //redirection côté serveur
+                //return $this->render('produit2.html.twig', ['produit'=>$produit]); //rendre le template produit 2
+                return $this->redirectToRoute('produit2', ['id_produit' => $id_produit]); //redirection côté client 
+            }
+        }
+        throw $this->createNotFoundException("Produit indisponible");
+    }
+/**
+     * @Route("/produit4/{id_produit}")
+     */
+    public function produit4Action($id_produit) {
+        if (filter_var($id_produit, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]])) {
+            $produit = $this->getDoctrine()
+                    ->getRepository("AppBundle:Produit")
+                    ->find($id_produit);
+            if ($produit) {
+            $em= $this->getDoctrine()->getManager();
+            $em->remove($produit);
+            $em->flush();
+                return $this->redirectToRoute('produit2', ['id_produit' => $id_produit]); 
+            }
+        }
+        throw $this->createNotFoundException("Produit indisponible");
+    }
 }
